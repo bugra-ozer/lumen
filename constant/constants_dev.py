f0e9c2c8a0375ab9618b8eba6df9416c07a550d2
bugra-ozer <bugraozer791@gmail.com>
2026-06-03 12:08:14 +0100

    fix: test module column string mismatch

--- constant/constants_dev.py
+++ constant/constants_dev.py
@@ -1,3 +1,5 @@
+from constant import constants as cons
+
 #CI
 
 MOCK_RANDOM_URL= '127.0.0.1:8000'
@@ -5,14 +7,14 @@ MOCK_RANDOM_PATH= 'giber/'
 MOCK_CONTENT_LENGTH={'Content-Length': '32768'} #dict
 MOCK_DATA_COMP_BYTES=[b'compressed', b'']
 MOCK_DATA_RESPONSE_BYTES=([b'a' * 8192, b'b' * 8192, b'c' * 8192, b'd' * 8192])
-DUMMY_DATAFRAME_COLUMNS_ONE=['IMDBid', 'Title']
-DUMMY_DATAFRAME_COLUMNS_TWO=['IMDBid', 'Genre']
+DUMMY_DATAFRAME_COLUMNS_ONE=[cons.IMDB_ID_COLUMN, cons.PRIMARY_TITLE_COLUMN]
+DUMMY_DATAFRAME_COLUMNS_TWO=[cons.IMDB_ID_COLUMN, cons.GENRE_COLUMN]
 DUMMY_DATAFRAME_DATA=[['4924', 'Shawshank Redemption'], ['2134', 'Rebellion']]
 MOCK_DATAFRAME_DATA=['tt1675434', '8.5', '1027907', 'The Intouchables', '2011', 'Comedy,Drama'],['tt7286456', '8.3', '1694585', 'Joker', '2019', 'Crime,Drama,Thriller'] # noqa
 DUMMY_PATH="/test/test.py"
 DUMMY_DATE="29-05-1823"
-DUMMY_FILTER_TOOLS={'Genre': {'value': ['action', 'horror']}}
-DUMMY_SAD_FILTER_TOOLS=['Genre',['action','horror']]
+DUMMY_FILTER_TOOLS={f'{cons.GENRE_COLUMN}': {'value': ['action', 'horror']}}
+DUMMY_SAD_FILTER_TOOLS=[f'{cons.GENRE_COLUMN}',['action','horror']]
 
 #api
 DUMMY_HASHED_PW= b"$2b$12$6Ik6AsvGpf9U3xm8aLhf8eB/fL1.EcgMauA58Mzfz5PbXLhNFmqWC"
