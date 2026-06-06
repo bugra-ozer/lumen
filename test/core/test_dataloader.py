76d337f23000554a49c42f6dafa05850f956e46b
bugra-ozer <bugraozer791@gmail.com>
2026-06-06 19:42:32 +0100

    fix: broken unit tests after db migration

--- test/core/test_dataloader.py
+++ test/core/test_dataloader.py
@@ -1,20 +1,23 @@
-import io, pytest, pandas as pd, pathlib as pl, logging
+import io, pytest, pandas as pd, pathlib as pl, logging, sqlalchemy
 from unittest.mock import MagicMock
 from constant import constants_dev as cons_dev
 from constant import constants as cons
 from unittest import mock
+from db.database import db_engine_local
 from main import DataLoader
+from db import database as db
 
 logger = logging.getLogger(__name__)
 patch = mock.patch
 
-@patch('main.pd.read_parquet')
-def test_read_parquet(mock_parquet):
+@patch('main.pd.read_sql')
+def test_read_sql(mock_sql):
     unit = DataLoader()
-    mock_parquet.return_value = pd.DataFrame()
-    path=pl.Path(cons_dev.MOCK_RANDOM_PATH)
-    read=unit.read_file(cons_dev.MOCK_RANDOM_PATH, cons.STR_PARQUET)
-    mock_parquet.assert_called_once_with(path)
+    mock_sql.return_value = pd.DataFrame()
+    table_name=cons.TABLE_NAME_CONTENT
+    read=unit.read_file(table_name, cons.STR_SQL)
+    mock_sql.assert_called_once_with(mock.ANY, db_engine_local)
+
     assert isinstance(read,pd.DataFrame)
 
 @patch('main.pd.read_csv')
@@ -22,17 +25,17 @@ def test_read_csv(mock_tsv, usecols=None):
     unit = DataLoader()
     mock_tsv.return_value = pd.DataFrame()
     path=pl.Path(cons_dev.MOCK_RANDOM_PATH)
-    read=unit.read_file(cons_dev.MOCK_RANDOM_PATH, 'tsv')
+    read=unit.read_file(cons_dev.MOCK_RANDOM_PATH, cons.STR_TSV)
     mock_tsv.assert_called_once_with(path, delimiter='\t', encoding='latin-1', on_bad_lines='skip', na_values='\\N', usecols=usecols)
     assert isinstance(read,pd.DataFrame)
 
-@patch('main.pd.DataFrame.to_parquet')
-def test_to_parquet(mock_to_parquet):
+@patch('main.pd.DataFrame.to_sql')
+def test_to_sql(mock_to_sql):
     unit = DataLoader()
-    mock_to_parquet.return_value = None
-    path=cons_dev.MOCK_RANDOM_PATH #raw string
-    write=unit.save_file(pd.DataFrame(),path)
-    mock_to_parquet.assert_called_once_with(path)
+    mock_to_sql.return_value = None
+    table_name=cons.TABLE_NAME_CONTENT #raw string
+    write=unit.save_file(pd.DataFrame(),table_name, cons.STR_SQL)
+    mock_to_sql.assert_called_once_with(mock.ANY, db_engine_local, if_exists='append', index=False)
     assert isinstance(write,DataLoader)
 
 def test_merge():
