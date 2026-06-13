993b2bbf5b47712a2530264f49ac020842fa532d
bugra-ozer <bugraozer791@gmail.com>
2026-06-13 15:41:40 +0100

    task: add unit test for Bayesian scoring

--- constant/constants_dev.py
+++ constant/constants_dev.py
@@ -16,6 +16,10 @@ DUMMY_DATE="29-05-1823"
 DUMMY_FILTER_TOOLS={f'{cons.GENRE_COLUMN}': {'value': ['action', 'horror']}}
 DUMMY_SAD_FILTER_TOOLS=[f'{cons.GENRE_COLUMN}',['action','horror']]
 
+#unit_test_bayes
+MOCK_DECAY_YEARS_OLD=cons.DECAY_FACTOR_THRESHOLD[0]
+MOCK_DECAY_VALUE=cons.DECAY_FACTOR_VALUES[0]
+
 #api
 DUMMY_HASHED_PW= b"$2b$12$6Ik6AsvGpf9U3xm8aLhf8eB/fL1.EcgMauA58Mzfz5PbXLhNFmqWC"
 USERS={"admin": b'$2b$12$Gy9z3lihHck5fCP4dAJMB.JzryhwuExZgHJ49GgynNW5t88hEuOLa', "robert55": b'$2b$12$AnnHZBLv63cVShZhl2OMjuUJX5fYKX4e23/LB8iWTV7aJzAHj5bxG'} # noqa
---
+++ test/scorer/test_scorer.py
@@ -0,0 +1,21 @@
+import io, pytest, logging, pandas as pd
+from unittest.mock import MagicMock
+from constant import constants as cons
+from constant import constants_dev as cons_dev
+from unittest import mock
+from scorer import bayesian_algorithm as bayes
+
+logger = logging.getLogger(__name__)
+
+def test_build_score_validify():
+    mock_record = cons_dev.MOCK_DATAFRAME_DATA[0][:5]
+    mock_record.pop(3)
+    mock_record[1]=float(mock_record[1])
+    mock_record[2]=float(mock_record[2])
+    mock_record[3]=int(mock_record[3])
+    df=pd.DataFrame(data=[mock_record],columns=[cons.IMDB_ID_COLUMN, cons.AVERAGE_RATING_COLUMN, cons.NUMBER_OF_VOTES_COLUMN, cons.PUBLISHED_COLUMN])
+    unit=bayes.BayesianScorer(df)
+    unit.score()
+    assert 0<unit.data[cons.DECAY_FACTOR_COLUMN][0]<1
+    assert unit.data[cons.BAYES_SCORE_COLUMN][0]==pytest.approx(mock_record[1])
+    assert unit.data[cons.ADJUSTED_SCORE_COLUMN][0]==unit.data[cons.DECAY_FACTOR_COLUMN][0]*unit.data[cons.BAYES_SCORE_COLUMN][0]
