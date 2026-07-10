b33ea53148f6f13b3ec9863e0f9d554bb4cf0785
bugra-ozer <bugraozer791@gmail.com>
2026-07-10 14:43:39 +0100

    docs: fix import convention

--- test/scorer/test_scorer.py
+++ test/scorer/test_scorer.py
@@ -3,7 +3,7 @@ from unittest.mock import MagicMock
 from constant import constants as cons
 from constant import constants_dev as cons_dev
 from unittest import mock
-from scorer import bayesian_algorithm as bayes
+from scorer.bayesian_algorithm import BayesianScorer
 
 logger = logging.getLogger(__name__)
 
@@ -15,7 +15,7 @@ def test_build_score_validify():
     mock_record[2]=float(mock_record[2])
     mock_record[3]=int(mock_record[3])
     df=pd.DataFrame(data=[mock_record],columns=[cons.IMDB_ID_COLUMN, cons.AVERAGE_RATING_COLUMN, cons.NUMBER_OF_VOTES_COLUMN, cons.PUBLISHED_COLUMN])
-    unit=bayes.BayesianScorer(df)
+    unit=BayesianScorer(df)
     unit.score()
     assert 0<unit.data[cons.DECAY_FACTOR_COLUMN][0]<1
     assert unit.data[cons.BAYES_SCORE_COLUMN][0]==pytest.approx(mock_record[1])
