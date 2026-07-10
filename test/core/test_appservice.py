9ec179d0649dc49ef8bcad602319a95ce786bd8d
bugra-ozer <bugraozer791@gmail.com>
2026-07-10 13:10:34 +0100

    test(AppService): add empty dict/None filter_tools tests

--- test/core/test_appservice.py
+++ test/core/test_appservice.py
@@ -1,11 +1,27 @@
-import pytest, unittest.mock as mock, sqlalchemy, pandas as pd, constant.constants_dev as cons_dev
-from main import AppService
+import pytest, unittest.mock as mock, sqlalchemy, pandas as pd, constant.constants as cons, constant.constants_dev as cons_dev, main
+from main import AppService, AppManager
+
+patcher = mock.patch
 
 @pytest.fixture
-def config():
+@patcher('main.DataContainer')
+@patcher('main.BayesianScorer')
+def config(scorer, container):
     uri = cons_dev.DUMMY_DB_URI
     engine = sqlalchemy.create_engine(uri)
     unit=AppService(engine)
-    unit.container=mock.Mock()
-    unit.bayes=mock.Mock()
-    return unit
+    local_user_id=1
+    return unit, local_user_id
+
+@patcher('main.DataFilter')
+def test_none_filter_tools(data_filter:mock.Mock, config):
+    unit, local_user_id = config
+    unit.run(None, local_user_id)
+    data_filter.assert_called_once_with(unit.data)
+
+@patcher('main.DataFilter')
+def test_empty_dict_filter_tools(data_filter:mock.Mock, config):
+    unit, local_user_id = config
+    unit.run({}, local_user_id)
+    data_filter.assert_called_once_with(unit.data)
+
