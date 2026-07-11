47b64c5a7d239f78b8cb811805056f02293f1528
bugra-ozer <bugraozer791@gmail.com>
2026-07-11 12:06:21 +0100

    test(AppService): add pick top method test

--- test/core/test_appservice.py
+++ test/core/test_appservice.py
@@ -1,12 +1,12 @@
 import pytest, unittest.mock as mock, sqlalchemy, pandas as pd, constant.constants as cons, constant.constants_dev as cons_dev, main
-from main import AppService, AppManager
+from main import AppService, AppManager, DataFilter
 
 patcher = mock.patch
 
 @pytest.fixture
 @patcher('main.DataContainer')
 @patcher('main.BayesianScorer')
-def config(scorer, container):
+def config(scorer, container): # noqa
     uri = cons_dev.DUMMY_DB_URI
     engine = sqlalchemy.create_engine(uri)
     unit=AppService(engine)
@@ -31,3 +31,10 @@ def test_valid_filter_tools(data_filter:mock.Mock, config):
     filter_tools=cons_dev.TEST_VALID_FILTER_TOOLS
     unit.run(filter_tools, local_user_id)
     data_filter.assert_called_once_with(unit.data, filter_tools)
+
+def test_pick_top(config):
+    unit, local_user_id = config
+    df=DataFilter(cons_dev.DUMMY_DATAFRAME_CONTENT_BAYES).result
+    result=unit._pick_top(df, 2, cons.N_POP, pd.DataFrame(columns=cons_dev.TEST_CONTENT_BAYES_COLUMNS, data=None))
+    assert cons_dev.DUMMY_RECORD_CONTENT_BAYES_SCORE_LOWEST[8] not in result[cons.ADJUSTED_SCORE_COLUMN].values
+    assert len(result) == cons.N_POP
